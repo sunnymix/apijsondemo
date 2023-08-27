a2a045cb0023d6e8521244003d58fae1a4e97c5a
sunny <sunny@sunnymix.com>
2023-08-27 12:37:28 +0800

    更新readme和gitignore

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-APIJSON Demo
+# APIJSON Demo
+
+## Docs
+
+- 官网：[apijson.cn](http://apijson.cn)
+- 文档：[apijson.cn/doc/zh/](http://apijson.cn/doc/zh/)
+- 入门：[APIJSON入门教程](https://github.com/Tencent/APIJSON/blob/master/%E8%AF%A6%E7%BB%86%E7%9A%84%E8%AF%B4%E6%98%8E%E6%96%87%E6%A1%A3.md)
+
+
---
+++ src/main/java/com/example/apijsondemo/ApijsondemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.apijsondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApijsondemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApijsondemoApplication.class, args);
+    }
+
+}
